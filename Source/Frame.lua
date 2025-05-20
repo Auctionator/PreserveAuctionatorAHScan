5b475e4fbf8941f60eec10a4a7dde9822347647f
6mot, Tom <tflo@users.noreply.github.com>
2025-05-20 02:25:00 +0200

    Auto-remove too old and unneeded scan tables

--- Source/Frame.lua
+++ Source/Frame.lua
@@ -10,13 +10,27 @@ function PreserveAuctionatorAHScanDataFrameMixin:ReceiveEvent(eventName, eventDa
   if Auctionator.FullScan and Auctionator.FullScan.Events and eventName == Auctionator.FullScan.Events.ScanComplete then
     PRESERVE_AUCTIONATOR_AH_SCAN_LAST_SCAN = {
       realm = Auctionator.Variables.GetConnectedRealmRoot(),
+      time = time(),
       data = eventData,
     }
+    PRESERVE_AUCTIONATOR_AH_SCAN_LAST_SUMMARY = nil
   elseif Auctionator.IncrementalScan and Auctionator.IncrementalScan.Events and eventName == Auctionator.IncrementalScan.Events.ScanComplete then
     PRESERVE_AUCTIONATOR_AH_SCAN_LAST_SUMMARY = {
       realm = Auctionator.Variables.GetConnectedRealmRoot(),
+      time = time(),
       data = eventData,
     }
+    PRESERVE_AUCTIONATOR_AH_SCAN_LAST_SCAN = nil
+  end
+end
+
+local function removeAgedData()
+  local now, maxAge = time(), 60 * 60
+  for _, v in ipairs({ "PRESERVE_AUCTIONATOR_AH_SCAN_LAST_SCAN", "PRESERVE_AUCTIONATOR_AH_SCAN_LAST_SUMMARY" }) do
+    if _G[v] and _G[v].data and (not _G[v].time or now - _G[v].time > maxAge) then
+      wipe(_G[v])
+      -- print(format("Preserve Auctionator AH Scan: Removed scan data for Collectionator that was over %d minutes old.", maxAge / 60))
+    end
   end
 end
 
@@ -34,4 +48,5 @@ function PreserveAuctionatorAHScanDataFrameMixin:OnEvent(event, ...)
       })
     end
   end
+  removeAgedData()
 end
